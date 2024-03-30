ef3ce75ac5d66d645cf520edeebb0a73390621a8
AdamOuld <ouldsfiyaadam@gmail.com>
2024-03-30 14:05:40 -0400

    https

--- src/logic.js
+++ src/logic.js
@@ -1,5 +1,5 @@
 
-const baseCall = "http://api.weatherapi.com/v1/current.json?key=8b0e1e1916d64ea9aa3192217242703&q="
+const baseCall = "https://api.weatherapi.com/v1/current.json?key=8b0e1e1916d64ea9aa3192217242703&q="
 
 
 async function fetchData(message) {
